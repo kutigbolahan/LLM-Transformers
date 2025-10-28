89bf314fc5999cd61c026a8e070be8cfd86e9942
kuti Gbolahan <kutigbolahan.ab@gmail.com>
2025-10-28 11:53:25 +0100

    added model name

--- transform.py
+++ transform.py
@@ -1,1 +1,7 @@
 from transformers import pipeline, AutoTokenizer, AutoModelForQuestionAnswering
+
+def create_simple_llm():
+    """
+    Creates a simple LLM using GPT-2 model
+    """
+    model_name= "distillgpt2"
