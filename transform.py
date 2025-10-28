6d002c8b56f74157350e06f3741b9213d7e6923c
kuti Gbolahan <kutigbolahan.ab@gmail.com>
2025-10-28 13:39:14 +0100

    Text generator pipeline

--- transform.py
+++ transform.py
@@ -4,4 +4,12 @@ def create_simple_llm():
     """
     Creates a simple LLM using GPT-2 model
     """
-    model_name= "distillgpt2"
+    model_name= "distilgpt2"
+    
+    generator = pipeline('text-generation', model=model_name,pad_token_id=50265)
+    return generator
+
+generator = create_simple_llm()
+prompt = 'once upon a time'
+generated_text = generator(prompt, max_length=1000,num_return_sequences=1,truncation=True)
+print(generated_text[0]['generated_text'])
